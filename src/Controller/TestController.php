587728544acf86f10af35823f6724766c4f6c37e
sefhirot69 <josalillo@gmail.com>
2022-03-19 11:25:29 +0100

    feat : instalamos twig y sus dependencias

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -11,9 +11,8 @@ class TestController extends AbstractController
     #[Route('/test', name: 'app_test')]
     public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TestController.php',
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
         ]);
     }
 }
