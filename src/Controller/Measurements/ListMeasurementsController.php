e82b3070bb444f0989fdbc39e558cfff3cf41bb4
josealiaslloret <jmalias@cash-converters.es>
2022-03-20 12:20:05 +0100

    feat : checkeamos el findAll de doctrine sobre la tabla de mediciones

--- src/Controller/Measurements/ListMeasurementsController.php
+++ src/Controller/Measurements/ListMeasurementsController.php
@@ -6,18 +6,20 @@ namespace App\Controller\Measurements;
 
 use App\Repository\DoctrineMeasurementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class ListMeasurementsController extends AbstractController
 {
-    public function __construct(private DoctrineMeasurementRepository $repository)
+    public function __construct(private DoctrineMeasurementRepository $measurementRepository)
     {
     }
 
     #[Route(path: '/list-measurements', name: 'app_list_measurements', methods: ['GET'])]
     public function __invoke(): Response
     {
-        return new Response('ok', Response::HTTP_OK);
+        $measurements = $this->measurementRepository->findAll();
+        return new JsonResponse($measurements, Response::HTTP_OK);
     }
 }
