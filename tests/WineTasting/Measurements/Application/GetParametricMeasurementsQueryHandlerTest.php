abe1fea72dd5ed7326a33e7ce7675936d6c8949c
josealiaslloret <jmalias@cash-converters.es>
2022-03-26 19:04:21 +0100

    feat : refactor getParametric

--- tests/WineTasting/Measurements/Application/GetParametricMeasurementsQueryHandlerTest.php
+++ tests/WineTasting/Measurements/Application/GetParametricMeasurementsQueryHandlerTest.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 
 namespace App\Tests\WineTasting\Measurements\Application;
 
+use App\Tests\Factory\MeasurementTypeDoctrineFactory;
+use App\Tests\Factory\VarietyTypeDoctrineFactory;
 use App\WineTasting\Measurements\Application\GetParametricMeasurementsQueryHandler;
 use App\WineTasting\Measurements\Application\ParametricMeasurementsDataSource;
-use App\WineTasting\Measurements\Domain\Dto\MeasurementTypeDto;
 use App\WineTasting\Measurements\Domain\Dto\ParametricMeasurementsDto;
-use App\WineTasting\Measurements\Domain\Dto\VarietyTypeDto;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Zenstruck\Foundry\Test\Factories;
 
 final class GetParametricMeasurementsQueryHandlerTest extends TestCase
 {
     private MockObject|ParametricMeasurementsDataSource $dataSourceMock;
+    use Factories;
 
     protected function setUp(): void
     {
@@ -27,14 +29,23 @@ final class GetParametricMeasurementsQueryHandlerTest extends TestCase
     public function shouldReturnParametricMeasurements(): void
     {
         // GIVEN
+        $measurementsType = [
+            MeasurementTypeDoctrineFactory::new()->create()->object(),
+            MeasurementTypeDoctrineFactory::new()->create()->object(),
+            MeasurementTypeDoctrineFactory::new()->create()->object(),
+        ];
+
+        $varietiesType = [
+            VarietyTypeDoctrineFactory::new()->create()->object(),
+            VarietyTypeDoctrineFactory::new()->create()->object(),
+            VarietyTypeDoctrineFactory::new()->create()->object(),
+        ];
+
         $this->dataSourceMock
             ->expects(self::once())
             ->method('getParams')
             ->willReturn(
-                ParametricMeasurementsDto::create(
-                    [MeasurementTypeDto::create(1, 'test')],
-                    [VarietyTypeDto::create(1, 'test')]
-                )
+                ParametricMeasurementsDto::create($measurementsType, $varietiesType)
             );
 
         $queryHandler = new GetParametricMeasurementsQueryHandler($this->dataSourceMock);
